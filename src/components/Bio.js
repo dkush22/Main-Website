f13561d95b1dd11f637171e234ca02bea69d34b5
dkush22 <dkush22@gmail.com>
2017-11-30 18:58:57 -0500

    pictures in bio but not fitting right

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -1,9 +1,15 @@
 import React from 'react'
 import familyPic from '../Family-Picture.jpg'
+import michPic from '../Mich-Picture.png'
+import dbPic from '../DB-Picture.png'
+import cubaPic from '../Cuba-Picture.png'
+import italyPic from '../Italy-Picture.png'
+import facebookPic from '../Facebook-Picture.png'
 
 
 const Bio = () => {
 	return (
+<div className="Body">
 <div className="ui grid">
   <div className="row">
     <div className="three wide column">
@@ -13,31 +19,33 @@ const Bio = () => {
      <h2>About Me</h2>
     </div>
     <div className="three wide column">
-      <img src={familyPic}/>
+      <img src={michPic}/>
     </div>
   </div>
   <div className="row">
     <div className="three wide column">
-      <img src={familyPic}/>
+      <img src={dbPic}/>
     </div>
     <div className="ten wide column">
       <h2>My Passion for Code</h2>
+      <p>Test</p>
     </div>
     <div className="three wide column">
-      <img src={familyPic}/>
+      <img src={facebookPic}/>
     </div>
   </div>
   <div className="row">
     <div className="three wide column">
-      <img src={familyPic}/>
+      <img src={cubaPic}/>
     </div>
     <div className="ten wide column">
       <h2>What's Next?</h2>
     </div>
     <div className="three wide column">
-      <img src={familyPic}/>
+      <img src={italyPic}/>
     </div>
   </div>
+</div>
 </div>
 )
 }
